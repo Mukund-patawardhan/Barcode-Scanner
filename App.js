0f81205015f38515c14dbca58bf26e391a7cc582
Mukund-patawardhan <61786847+Mukund-patawardhan@users.noreply.github.com>
2020-11-20 18:50:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Scanner from './Screens/Scanner';
+
+export default class App extends React.Component{
+    render(){
+        return(
+            <Scanner />
+        )
+    }
+}
